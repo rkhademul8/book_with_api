df12d522a429afaf5baa9ccfa4cf29de0915ad46
rkhademul8 <rkhademul8.com>
2022-02-17 00:02:07 +0600

    spinner_and_somw_error_handle_done

--- js/api.js
+++ js/api.js
@@ -1,13 +1,35 @@
 
+// spinner function
+const toggleSpinner=displayStyle=>{
+    document.getElementById('spinner').style.display=displayStyle
+}
+
+
+
 // fetch data from API function
 
 const dataLoad=()=>{
     const searchText=document.getElementById('search-field').value
     document.getElementById('search-field').value=''
 
+    if(searchText==''){
+        alert('Sorry')
+    }
+    else if(searchText<0){
+        alert('Sorry')
+    }
+
+   else{
+        // spinner function call
+    toggleSpinner('block')
+
     fetch(`https://openlibrary.org/search.json?q=${searchText}`)
     .then(res=>res.json())
     .then(data=>displayBook(data.docs))
+   }
+
+    
+    
 
 }
 
@@ -15,12 +37,15 @@ const dataLoad=()=>{
 
 const displayBook=data=>{
     const displaybook=document.getElementById('display-book')
+
+    // remove previous data
     displaybook.textContent=''
-    
+
+    //  spinner function call
+    toggleSpinner('none') 
+
     data.forEach(info=>{
-
         console.log(info)
-
         if(info.cover_i){
         const div=document.createElement('div')
         div.innerHTML=`
@@ -32,9 +57,7 @@ const displayBook=data=>{
             <h1 class=" text-1xl mt-1">Author Name:${info.author_name[0]}</h1>
             <h1 class=" text-1xl mt-1">Publisher:${info.publisher[0]}</h1>
             <h1 class=" text-1xl mt-1">Publish date:${info.publish_date[0]}</h1>
-            
-    
-    
+               
             <div class="flex justify-between mt-3">
                 <h1 class=" text-1xl">Price</h1>
                 <button class="bg-teal-600 p-1 text-white rounded-md text-1xl">Buy Name </button>
@@ -42,10 +65,12 @@ const displayBook=data=>{
         </div>
         `
         displaybook.appendChild(div)
-        }
-
-       
-      
         
+        }   
+          
     })
+    
+    
+
+    toggleSearchResult('block')
 }
